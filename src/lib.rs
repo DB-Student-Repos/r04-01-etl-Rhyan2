b10353cb691e8261aa8194d957b9d41d167311a6
Utman <148863405+Rhyan2@users.noreply.github.com>
2024-05-01 13:57:29 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, result};
+pub fn transform(i: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    let mut map=BTreeMap::new();
+    for (key,chars) in i{
+        for c in chars.iter().flat_map(|x| x.to_lowercase()) {
+            map.insert(c,*key);
+        }
 
-pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {h:?}?")
-}
+        }
+    map
+    }
